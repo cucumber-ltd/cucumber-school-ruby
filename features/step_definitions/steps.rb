8c724e18bbec2cd0d7c9ff4f88af27c89fcd6e30
Matt Wynne <matt@mattwynne.net>
2014-11-22 22:00:49 +0000

    Add generated step definitions

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,11 @@
+Given(/^Lucy is (\d+)m from Sean$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^Sean shouts "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^Lucy hears Sean's message$/) do
+  pending # express the regexp above with the code you wish you had
+end
