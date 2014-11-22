e6afb973765a82a4ed2f3f9e9ed231cc56167d5f
Matt Wynne <matt@mattwynne.net>
2014-11-22 22:05:29 +0000

    Have Sean try to shout from the step definitions

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -2,12 +2,12 @@ require 'shouty'
 
 Given(/^Lucy is (\d+)m from Sean$/) do |distance|
   lucy = Person.new
-  sean = Person.new
+  @sean = Person.new
   lucy.move_to(distance)
 end
 
-When(/^Sean shouts "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^Sean shouts "(.*?)"$/) do |message|
+  @sean.shout(message)
 end
 
 Then(/^Lucy hears Sean's message$/) do
