8fe75f8aa5018dd80db6df3cccedc7e4d2315139
Matt Wynne <matt@mattwynne.net>
2014-11-22 23:03:22 +0000

    Expect Person instances to subscribe to the network

--- spec/shouty/person_spec.rb
+++ spec/shouty/person_spec.rb
@@ -1,4 +1,11 @@
 require 'shouty'
 
 describe Person do
+  let(:network) { double.as_null_object }
+
+  it "subscribes to the network" do
+    expect(network).to receive(:subscribe)
+    sean = Person.new(network)
+  end
+
 end
