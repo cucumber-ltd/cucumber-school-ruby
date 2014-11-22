e0cca5eb84d72ecb8a791f7488ade3f1cd1f6bd1
Matt Wynne <matt@mattwynne.net>
2014-11-22 23:07:00 +0000

    Make that test pass - broadcast shouts to network

--- lib/shouty.rb
+++ lib/shouty.rb
@@ -1,12 +1,14 @@
 class Person
   def initialize(network)
     network.subscribe(self)
+    @network = network
   end
 
   def move_to(location)
   end
 
   def shout(message)
+    @network.broadcast(message)
   end
 
   def messages_heard
