c982f47a0f75ca2baa95d8fe82d499c38d0a5193
Matt Wynne <matt@mattwynne.net>
2014-11-22 22:07:21 +0000

    Start driving out the assertion

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,15 +1,16 @@
 require 'shouty'
 
 Given(/^Lucy is (\d+)m from Sean$/) do |distance|
-  lucy = Person.new
+  @lucy = Person.new
   @sean = Person.new
-  lucy.move_to(distance)
+  @lucy.move_to(distance)
 end
 
 When(/^Sean shouts "(.*?)"$/) do |message|
   @sean.shout(message)
+  @message_from_sean = message
 end
 
 Then(/^Lucy hears Sean's message$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(@lucy.messages_heard).to include @message_from_sean
 end
