eeccc46b94f3b1fadd6a6b056262cb917e85e886
Matt Wynne <matt@mattwynne.net>
2014-11-23 14:18:38 +0000

    Remove old step and fetch Sean from hash

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -9,12 +9,8 @@ Given(/^a person named (\w+)$/) do |name|
   @people[name] = Person.new(@network)
 end
 
-Given(/^a person named Sean$/) do
-  @sean = Person.new(@network)
-end
-
 When(/^Sean shouts "(.*?)"$/) do |message|
-  @sean.shout(message)
+  @people['Sean'].shout(message)
   @message_from_sean = message
 end
 
