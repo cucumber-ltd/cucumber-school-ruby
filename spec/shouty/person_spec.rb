130b604928b32cda877e441ad289791bda915aa9
Matt Wynne <matt@mattwynne.net>
2014-12-09 13:21:56 +0000

    Make the test pass

--- spec/shouty/person_spec.rb
+++ spec/shouty/person_spec.rb
@@ -1,23 +1,31 @@
 require 'shouty'
 
 describe Person do
+  let(:sean) { Person.new(network, 0) }
+  let(:lucy) { Person.new(network, 100) }
   let(:network) { double.as_null_object }
 
   it "subscribes to the network" do
     expect(network).to receive(:subscribe)
-    sean = Person.new(network)
+    location = 0
+    new_person = Person.new(network, location)
+  end
+
+  it "has a location" do
+    expect(lucy.location).to eq 100
   end
 
   it "broadcasts shouts to the network" do
     message = "Free bagels!"
-    sean = Person.new(network)
-    expect(network).to receive(:broadcast).with message
+    sean_location = 0
+    expect(network).to receive(:broadcast).with message, sean_location
     sean.shout message
   end
 
   it "remembers messages heard" do
     message = "Free bagels!"
-    lucy = Person.new(network)
+    location = 100
+    lucy = Person.new(network, location)
     lucy.hear(message)
     expect(lucy.messages_heard).to eq [message]
   end
