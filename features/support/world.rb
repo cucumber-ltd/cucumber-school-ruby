4418da7fe1e5b98ba8439585c028deb38fbcd2ad
Matt Wynne <matt@mattwynne.net>
2015-06-01 10:13:40 +0100

    Add two Worlds
    We'll use this to drive out the main behaviour of the web app, choosing
    a different world module to drive the app via the web.
    
    Run the web scenario using the following command:
    
    `shouty_test_depth=web cucumber`
    
    You can stil run the domain-level tests as normal:
    
    `cucumber`

--- features/support/world.rb
+++ features/support/world.rb
@@ -1,4 +1,4 @@
-module ShoutyWorld
+module DomainWorld
 
   def people
     @people ||= {}
@@ -16,4 +16,36 @@ module ShoutyWorld
 
 end
 
-World(ShoutyWorld)
+module WebWorld
+
+  def people
+    @people ||= {}
+  end
+
+  def messages_shouted_by
+    @messages_shouted_by ||= {}
+  end
+
+  def sean_shout(message)
+    log_in_as 'Sean'
+    shout message
+    messages_shouted_by['Sean'] ||= []
+    messages_shouted_by['Sean'] << message
+  end
+
+  private
+
+  def log_in_as(name)
+    warn "TODO: implement log_in_as"
+  end
+
+  def shout(message)
+    warn "TODO: implement shout"
+  end
+end
+
+if ENV['shouty_test_depth'] == "web"
+  World(WebWorld)
+else
+  World(DomainWorld)
+end
