e7d0187fddf0ff59df9cc3625906b02f047c6471
Matt Wynne <matt@mattwynne.net>
2015-06-12 10:48:47 -0700

    REFACTOR: Use #messages_heard_by everywhere

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -58,18 +58,17 @@ Then(/^Lucy hears Sean's message$/) do
 end
 
 Then(/^(Larry|Lucy) does not hear Sean's message$/) do |listener_name|
-  expect(people[listener_name].messages_heard).to_not include messages_shouted_by['Sean'].last
+  expect(messages_heard_by(listener_name)).to_not include messages_shouted_by['Sean'].last
 end
 
 Then(/^nobody hears Sean's message$/) do
-  people.values.each do |person|
-    expect(person.messages_heard).to_not include messages_shouted_by['Sean'].last
+  people.keys.each do |name|
+    expect(messages_heard_by(name)).to_not include messages_shouted_by['Sean'].last
   end
 end
 
 Then(/^Lucy hears the following messages:$/) do |expected_messages|
-  lucy = people['Lucy']
-  actual_messages = lucy.messages_heard.map { |message| [ message ] }
+  actual_messages = messages_heard_by('Lucy').map { |message| [ message ] }
   expected_messages.diff!(actual_messages)
 end
 
@@ -79,13 +78,13 @@ end
 
 Then(/^Larry hears both Sean's messages$/) do
   messages_shouted_by['Sean'].each do |message|
-    expect(people['Larry'].messages_heard).to include message
+    expect(messages_heard_by('Larry')).to include message
   end
 end
 
 Then(/^Lucy hears all Sean's messages$/) do
   messages_shouted_by['Sean'].each do |message|
-    expect(people['Lucy'].messages_heard).to include message
+    expect(messages_heard_by('Lucy')).to include message
   end
 end
 
