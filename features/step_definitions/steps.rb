f85b243d2f527a0b5765d2e7124a2484c4f516b3
Matt Wynne <matt@mattwynne.net>
2020-01-31 12:15:52 +0000

    Add step defs
    `bundle exec cucumber`

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,11 @@
+Given("Lucy is located {int}m from Sean") do |int|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("Sean shouts {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("Lucy hears Sean's message") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
